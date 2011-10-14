28d93c142cb25e5647efc7742a29fa0a27a59a2f
Christopher De Cairos <chris.decairos@gmail.com>
2011-10-14 13:40:21 -0400

    ignoring the new, auto-generated manifest.json file

--- make-build-tool.py
+++ make-build-tool.py
@@ -10,6 +10,8 @@ playerdist = dist + "/players"
 moduledist = dist + "/modules"
 effectdist = dist + "/effects"
 
+manifestText = "{"
+
 if not os.path.exists( dist ):
   os.mkdir( dist )
 
@@ -35,7 +37,7 @@ def checkPath( path ):
   if not os.path.exists( path ):
     os.mkdir( path )
 
-def process( root, filename, dist ):
+def process( root, filename, dist, m ):
   if filename.endswith( ".md" ):
     md = markdown.Markdown()
     fullpath = safePath( os.path.abspath( os.path.join( root, filename )))
@@ -54,6 +56,7 @@ def process( root, filename, dist ):
 
     FILE.write( html )
     FILE.close()
+    return ""
 
   if filename.endswith( ".js" ) \
       and not filename.endswith( ".unit.js" ):
@@ -64,43 +67,69 @@ def process( root, filename, dist ):
     checkPath( distpath )
 
     shutil.copy( fullpath, distpath )
+    return '    "' + folder + '/' + filename + '",\n'
 
 print "Building dist/plugins ..."
+manifestText = manifestText + '\n  "plugins": [\n'
 for root, dirs, filenames in os.walk( "./plugins" ):
   for filename in filenames:
     sys.stdout.flush()
     sys.stderr.flush()
 
-    process( root, filename, plugindist )
+    text = process( root, filename, plugindist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
 
 print "Building dist/parsers/ ..."
+manifestText = manifestText[:len( manifestText ) - 2 ]  + '\n  ],\n  "parsers": [\n'
 for root, dirs, filenames in os.walk( "./parsers" ):
   for filename in filenames:
     sys.stdout.flush()
     sys.stderr.flush()
 
-    process( root, filename, parserdist )
+    text = process( root, filename, parserdist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
 
 print "Building dist/players/ ..."
+manifestText = manifestText[:len( manifestText ) - 2 ]  + '\n  ],\n  "players": [\n'
 for root, dirs, filenames in os.walk( "./players" ):
   for filename in filenames:
     sys.stdout.flush()
     sys.stderr.flush()
 
-    process( root, filename, playerdist )
+    text = process( root, filename, playerdist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
 
 print "Building dist/modules/ ..."
+manifestText = manifestText[:len( manifestText ) - 2 ]  + '\n  ],\n  "modules": [\n'
 for root, dirs, filenames in os.walk( "./modules" ):
   for filename in filenames:
     sys.stdout.flush()
     sys.stderr.flush()
 
-    process( root, filename, moduledist )
+    text = process( root, filename, moduledist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
 
 print "Building dist/effects/ ..."
+manifestText = manifestText[:len( manifestText ) - 2 ]  + '\n  ], \n  "effects": [\n'
 for root, dirs, filenames in os.walk( "./effects" ):
   for filename in filenames:
     sys.stdout.flush()
     sys.stderr.flush()
 
-    process( root, filename, effectdist )
+    text = process( root, filename, effectdist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
+
+manifestText = manifestText[:len( manifestText ) - 2 ]  + '\n  ]\n}'
+
+if os.path.isfile( "./manifest.json" ):
+  os.remove( "./manifest.json" )
+
+print "writing new manifest.json"
+manifest = open( "./manifest.json", "w" )
+manifest.write( manifestText )
+manifest.close()
